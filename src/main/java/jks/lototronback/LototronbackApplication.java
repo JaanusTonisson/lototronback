b8e2eaa2e5855ea2d98b7cda142d7f4b50c08606
Jaanus <tonisson.jaanus@gmail.com>
2025-02-28 16:19:21 +0200

    Initial commit

---
+++ src/main/java/jks/lototronback/LototronbackApplication.java
@@ -0,0 +1,13 @@
+package jks.lototronback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LototronbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LototronbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jks/lototronback/LototronbackApplicationTests.java
@@ -0,0 +1,13 @@
+package jks.lototronback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LototronbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
